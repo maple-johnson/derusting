70f7b5a33cfbbe93cba1c42dc31c26b4a5580187
Maple <135763163+maple-johnson@users.noreply.github.com>
2025-01-13 13:49:19 -0800

    Completed StringPractice.

--- src/StringPractice.java
+++ src/StringPractice.java
@@ -1,25 +1,53 @@
-public class StringPractice {
-  public static void main(String[] args) {
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringPractice 
+{
+  public static void main(String[] args) 
+  {
     // Create a string with at least 5 characters and assign it to a variable
+    String stringPractice = "Coding";
+    System.out.println(stringPractice);
 
     // Find the length of the string
+    System.out.println(stringPractice.length());
 
     // Concatenate (add) two strings together and reassign the result
+    stringPractice = stringPractice + " Java";
+    System.out.println(stringPractice);
 
     // Find the value of the character at index 3
+    System.out.println(stringPractice.charAt(3));
 
     // Check whether the string contains a given substring (i.e. does the string have "abc" in it?)
+    System.out.println(stringPractice.contains("Java"));
 
     // Iterate over the characters of the string, printing each one on a separate line
+    for (int i = 0; i < stringPractice.length(); i++) 
+    {
+          System.out.println(stringPractice.charAt(i));
+    }
 
     // Create an ArrayList of Strings and assign it to a variable
+    List<String> listStrings = new ArrayList<String>();
+    System.out.println(listStrings);
 
     // Add multiple strings to the List (OK to do one-by-one)
+    listStrings.add(stringPractice);
+    listStrings.add("Coding C++");
+    listStrings.add("Coding C#");
+    listStrings.add("Coding Python");
+
+    System.out.println(listStrings);
 
     // Join all of the strings in the list together into a single string separated by commas
     // Use a built-in method to achieve this instead of using a loop
+    String comp = listStrings.toString();
+    System.out.println(comp);
 
     // Check whether two strings are equal
+    System.out.println(comp.equals(listStrings.toString()));
+    System.out.println(listStrings.toString().equals(comp));
 
     /*
      * Reminder!
@@ -29,4 +57,5 @@ public class StringPractice {
      * We use == when comparing primitives (e.g. int or char).
      */
   }
+
 }
