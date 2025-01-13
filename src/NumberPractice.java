0cff074b76316b4efeced04689693a57203849de
Maple <135763163+maple-johnson@users.noreply.github.com>
2025-01-13 11:33:55 -0800

    Completed NumberPractice, and updated the toRefresh file

--- src/NumberPractice.java
+++ src/NumberPractice.java
@@ -1,17 +1,33 @@
-public class NumberPractice {
-  public static void main(String args[]) {
+public class NumberPractice 
+{
+  public static void main(String args[]) 
+  {
     // Create a float with a negative value and assign it to a variable
+    float negNum = -5.666f;
+    System.out.println(negNum);
 
     // Create an int with a positive value and assign it to a variable
+    int posNum = 7;
+    System.out.println(posNum);
 
     // Use the modulo % operator to find the remainder when the int is divided by 3
+    System.out.println(posNum % 3);
 
     // Use the modulo % operator to determine whether the number is even
     // (A number is even if it has a remainder of zero when divided by 2)
     // Use an if-else to print "Even" if the number is even and "Odd"
     // if the number is odd.
+    if (posNum % 2 == 0) 
+    {
+          System.out.println("Even");
+    }
+    else
+    {
+          System.out.println("Odd");
+    }
 
     // Divide the number by another number using integer division
+    System.out.println(posNum / 2);
 
     /*
      * Reminder!
@@ -22,4 +38,5 @@ public class NumberPractice {
      */
 
   }
+
 }
