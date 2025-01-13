8ebaac9b34a4949d8d3527d8ba3e45447f4179fb
Maple <135763163+maple-johnson@users.noreply.github.com>
2025-01-13 14:01:53 -0800

    Completed SetPractice.

--- src/SetPractice.java
+++ src/SetPractice.java
@@ -1,17 +1,37 @@
-public class SetPractice {
-  public static void main(String[] args) {
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetPractice 
+{
+  public static void main(String[] args) 
+  {
     // Create a HashSet of Strings and assign it to a variable of type Set
+    Set<String> hashItems =  new HashSet<String>();
+    System.out.println(hashItems);
 
     // Add 3 elements to the set
     // (It's OK to do it one-by-one)
+    hashItems.add("One");
+    hashItems.add("Two");
+    hashItems.add("Three");
+
+    System.out.println(hashItems);
 
     // Check whether the Set contains a given String
+    System.out.println(hashItems.contains("Two"));
 
     // Remove an element from the Set
+    hashItems.remove("Two");
+    System.out.println(hashItems);
 
     // Get the size of the Set
+    System.out.println(hashItems.size());
 
     // Iterate over the elements of the Set, printing each one on a separate line
+    for (String items : hashItems) 
+    {
+          System.out.println(items);
+    }
 
     /*
      * Warning!
@@ -26,4 +46,5 @@ public class SetPractice {
      * Also remember that sets do NOT have duplicates.
      */
   }
+
 }
